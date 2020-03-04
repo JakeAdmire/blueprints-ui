b83f440baf63a249442c64a4ec515428ff6bfcdf
JakeAdmire <jakeadmire1@gmail.com>
2020-03-03 20:00:20 -0700

    Convert default component to master export file

--- example/src/App.js
+++ example/src/App.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 
-import ExampleComponent from 'blueprints-ui'
+import { Button } from 'blueprints-ui'
 
 export default class App extends Component {
   render () {
     return (
       <div>
-        <ExampleComponent text='Modern React component module' />
+        <Button text='Click Me!' toggleable={true} variant="neumorphism" />
       </div>
     )
   }
