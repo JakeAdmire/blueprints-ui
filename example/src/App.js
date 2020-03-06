14e0b6b516649b6d0f5177fa68dc7a2b010d975d
JakeAdmire <jakeadmire1@gmail.com>
2020-03-06 16:06:55 -0700

    Updated example implementation

--- example/src/App.js
+++ example/src/App.js
@@ -5,9 +5,7 @@ import { Button } from 'blueprints-ui'
 export default class App extends Component {
   render () {
     return (
-      <div>
-        <Button text='Click Me!' toggleable={true} variant="neumorphism" />
-      </div>
+      <Button text='Click Me!' toggleable={true} />
     )
   }
 }
