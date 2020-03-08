ec1ce87d808f077a2b89eb3c95b0c6e2c49521ce
JakeAdmire <jakeadmire1@gmail.com>
2020-03-08 17:55:24 -0600

    Update example implementation

--- example/src/App.js
+++ example/src/App.js
@@ -3,9 +3,16 @@ import React, { Component } from 'react'
 import { Button } from 'blueprints-ui'
 
 export default class App extends Component {
+
+  handleClick(e) {
+    console.log(e.target);
+  }
+
   render () {
     return (
-      <Button text='Click Me!' toggleable={true} />
+      <Button text='Click Me!' 
+              toggleable
+              onClick={this.handleClick} />
     )
   }
 }
