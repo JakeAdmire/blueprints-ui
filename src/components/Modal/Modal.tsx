4625a83a5d6f9f4948a14428b2d0497b6f960ac9
JakeAdmire <jakeadmire1@gmail.com>
2020-04-07 13:36:20 -0600

    [enhancement]: Set base sizing/positioning rules

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,22 +1,48 @@
-import React, { Component, MouseEvent } from "react";
+import React, { Component, MouseEvent, FunctionComponent, useRef, useEffect, useState } from "react";
+import styles from './styles.css';
+import { createUseStyles } from "react-jss";
+
+export type ModalProps = { };
 
 
-export type ModalProps = { } & Partial<DefaultProps>;
+const useStyles = createUseStyles({
+    modal: (props: any) => {
+        const   modalWidth  = props.modalWidth,
+                modalHeight = props.modalHeight;
 
-type DefaultProps = { }
-
-type ModalState = { }
-
-export class Button extends Component<ModalProps, ModalState> {
-    constructor(props: ModalProps) {
-        super(props);
-        this.state = { }
+        return {
+            top: `calc(50% - ${modalHeight / 2}px)`,
+            left: `calc(50% - ${modalWidth / 2}px)`
+        }
     }
-    static defaultProps: DefaultProps = { }
+})
 
-    render() {
-        return (
-            <div></div>
-        )
+export const Modal: FunctionComponent<ModalProps> = (props) => {
+    const   [modalSize, setModalSize]   = useState({ modalWidth: 0, modalHeight: 0 }),
+            modalRef                    = useRef(null),
+            classes                     = useStyles({...props, ...modalSize});
+
+    useEffect(() => {
+        getModalSize();
+    }, []);
+
+    function getModalSize() {
+        setModalSize({
+            modalWidth: modalRef.current.offsetWidth,
+            modalHeight: modalRef.current.offsetHeight
+        })
     }
+
+    return (
+        <div className={styles.modalWrapper}>
+            <div    className={styles.modal + ' ' + classes.modal}
+                    ref={modalRef}>
+                {props.children}
+            </div>
+        </div>
+    );
+}
+
+Modal.defaultProps = {
+
 }
